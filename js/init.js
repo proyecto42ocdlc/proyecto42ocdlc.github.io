1252e087e03346dd912ac2e00be2fefa09e5c72d
proyecto42ocdlc <81542446+proyecto42ocdlc@users.noreply.github.com>
2021-03-28 19:54:04 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyCvOPgyS0nLHWeGzWVaYcA8xFHzJE51aHA",
+    authDomain: "proyecto42ocdlc.firebaseapp.com",
+    projectId: "proyecto42ocdlc",
+    storageBucket: "proyecto42ocdlc.appspot.com",
+    messagingSenderId: "139493395911",
+    appId: "1:139493395911:web:2670add608277e57285de2",
+    measurementId: "G-NEMTG7GP5Q"
+    });
