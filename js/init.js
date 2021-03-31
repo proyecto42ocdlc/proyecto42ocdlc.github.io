3c04d7c14102c19b5c11578a88574366163040d1
proyecto42ocdlc <81542446+proyecto42ocdlc@users.noreply.github.com>
2021-03-31 00:32:27 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAGX-o7N7fbaaJtnCg6wmbHSE50Qy1NY6I",
+    authDomain: "chat42oc.firebaseapp.com",
+    projectId: "chat42oc",
+    storageBucket: "chat42oc.appspot.com",
+    messagingSenderId: "300597237358",
+    appId: "1:300597237358:web:9659657f547a3f5652b87f",
+    measurementId: "G-6CBEY4JM4H"
+    });
