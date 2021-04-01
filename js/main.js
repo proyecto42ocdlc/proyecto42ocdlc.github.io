93a2fc12bd50101efa1b2e9fc78695d30d29c8ed
proyecto42ocdlc <81542446+proyecto42ocdlc@users.noreply.github.com>
2021-04-01 01:31:09 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 //       if (result.credential) {
 //         /** @type {firebase.auth.OAuthCredential} */
 //         var credential = result.credential;
-  
+
 //         // This gives you a Google Access Token. You can use it to access the Google API.
 //         var token = credential.accessToken;
 //         // ...
@@ -40,7 +40,7 @@
 // }
 
 
-function sesion(){
+function sesion() {
 
 
   // @ts-check
@@ -78,31 +78,31 @@ function sesion(){
 
 }
 
-  function agrega() {
-        /** Conexión a la base de datos. */
+function agrega() {
+  /** Conexión a la base de datos. */
   // // @ts-ignore
   // const firestore = firebase.firestore();
   // /** Agrega un usuario a la base de datos. */
 
-    const tel = document.getElementById("telefono").value;
-    const nom = document.getElementById("nombre").value;
-    const matri = document.getElementById("matricula").value;
-    const grupo = document.getElementById("grupo").value;
-    const fecha = document.getElementById("fecha").value;
-    /* "MENSAJE" es el nombre de la colección a la que se agregan los datos.
-     * "USUARIO", "TEXTO" y "BORRARALO" son los nombres de los campos en el
-     * documento.
-     * El timestamp contiene la fecha y hora en que se agrega el registro.*/
-    
+  const tel = document.getElementById("telefono").value;
+  const nom = document.getElementById("nombre").value;
+  const matri = document.getElementById("matricula").value;
+  const grupo = document.getElementById("grupo").value;
+  const fecha = document.getElementById("fecha").value;
+  /* "MENSAJE" es el nombre de la colección a la que se agregan los datos.
+   * "USUARIO", "TEXTO" y "BORRARALO" son los nombres de los campos en el
+   * documento.
+   * El timestamp contiene la fecha y hora en que se agrega el registro.*/
 
-    
+
+
 
 
   //   });
-// Initialize Cloud Firestore through Firebase
+  // Initialize Cloud Firestore through Firebase
 
 
-var db = firebase.firestore();
+  var db = firebase.firestore();
 
   db.collection("Alumnos").add({
     TELEFONO: tel,
@@ -110,101 +110,141 @@ var db = firebase.firestore();
     MATRICULA: matri,
     GRUPO: grupo,
     FECHA: fecha
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
-});
-  }
-  /** Muestra los mensaje almacenados en la collection "MENSAJE". Se
-   * actualiza automáticamente. */
+  })
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+}
+/** Muestra los mensaje almacenados en la collection "MENSAJE". Se
+ * actualiza automáticamente. */
 
- 
-    var db = firebase.firestore();
 
-    
-    /* Consulta que se actualiza automáticamente. Pide todos los registros
-     * de la colección "MENSAJE" ordenador por el campo "BORRARALO" de forma
-     * descendiente. */
-    db.collection("Alumnos")
-      .onSnapshot(
-        /** Función que muestra los datos enviados por el servidor. Si los
-         * datos cambian en el servidor, se vuelve a invocar esta función y
-         * recibe los datos actualizados.
-         * @param {Array} querySnapshot estructura parecida a un Array, que
-         * contiene una copia de los datos en el servidor. */
-        querySnapshot => {
-          // Vacía la lista con los mensajes.
-          // @ts-ignore
-          var salida = document.getElementById('salida');
-          salida.innerHTML = " ";
-          /* Revisa un por uno los registros de la consulta y los muestra.
-           * El iterador "doc" es un registro de la base de datos. */
-          querySnapshot.forEach(doc => {
-            // recupera los datos almacenados en el registro.
-            const data = doc.data();
-            
-            salida.innerHTML += /* html */
-              `<td>${doc.id} </td>
+var db = firebase.firestore();
+
+
+/* Consulta que se actualiza automáticamente. Pide todos los registros
+ * de la colección "MENSAJE" ordenador por el campo "BORRARALO" de forma
+ * descendiente. */
+db.collection("Alumnos")
+  .onSnapshot(
+    /** Función que muestra los datos enviados por el servidor. Si los
+     * datos cambian en el servidor, se vuelve a invocar esta función y
+     * recibe los datos actualizados.
+     * @param {Array} querySnapshot estructura parecida a un Array, que
+     * contiene una copia de los datos en el servidor. */
+    querySnapshot => {
+      // Vacía la lista con los mensajes.
+      // @ts-ignore
+      var salida = document.getElementById('salida');
+      salida.innerHTML = " ";
+      /* Revisa un por uno los registros de la consulta y los muestra.
+       * El iterador "doc" es un registro de la base de datos. */
+      querySnapshot.forEach(doc => {
+        // recupera los datos almacenados en el registro.
+        const data = doc.data();
+
+        salida.innerHTML += /* html */
+          `<td>${doc.id} </td>
               <td>${doc.data().NOMBRE} </td>
               <td>${doc.data().MATRICULA} </td>
               <td>${doc.data().GRUPO} </td>
               <td>${doc.data().TELEFONO} </td>
               <td>${doc.data().FECHA} </td>
               <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')" >Eliminar</button></td>
-              <td><button class="btn btn-warning">Editar</button></td>`;
-              
+              <td><button class="btn btn-warning" onclick="editar('${doc.id}','${doc.data().NOMBRE}','${doc.data().MATRICULA}','${doc.data().GRUPO}','${doc.data().TELEFONO}')">Editar</button></td>`;
 
 
-          })
-        },
-        /* Función que se invoca cuando hay un error. Muesstra el error. Al
-         * invocar esta función la conexión se cancela. Por lo mismo, se
-         * vuelve a conectar. */
-        e => {
-          procesaError(e);
-          // Intenta conectarse otra vez.
-          muestraMensajes();
-        }
-      )
-  
 
-
+      })
+    },
+    /* Función que se invoca cuando hay un error. Muesstra el error. Al
+     * invocar esta función la conexión se cancela. Por lo mismo, se
+     * vuelve a conectar. */
+    e => {
+      procesaError(e);
+      // Intenta conectarse otra vez.
+      muestraMensajes();
+    }
+  )
 
 
 
 
-  /** Procesa un error. Muestra el objeto en la consola y un cuadro de
-   * alerta con el mensaje.
-   * @param {Error} e descripción del error. */
-  function procesaError(e) {
-    console.log(e);
-    alert(e.message);
+
+
+
+/** Procesa un error. Muestra el objeto en la consola y un cuadro de
+ * alerta con el mensaje.
+ * @param {Error} e descripción del error. */
+function procesaError(e) {
+  console.log(e);
+  alert(e.message);
+}
+
+/** Map que contiene el texto de escape de los caracteres especiales de
+ * HTML.
+ * @type {Readonly<Map<string, string>> } */
+const codMap = Object.freeze(new Map([['&', '&amp;'], ['<', '&lt;'],
+['>', '&gt;'], ['"', '&quot;'], ["'", '&#039;']]));
+
+/** Codifica un texto para que escape los caracteres especiales y no se
+ * pueda interpretar como HTML. Esta técnica evita la inyección de código.
+ * @param {string} texto
+ * @returns {string} un texto que no puede interpretarse como HTML. */
+function cod(texto) {
+  return (texto || "").replace(/[&<>"']/g, letra => codMap.get(letra));
+}
+
+
+function eliminar(id) {
+  db.collection("Alumnos").doc(id).delete().then(function () {
+    console.log("Se borro el documento");
+  }).catch(function (error) {
+    console.log("Error al borrar el documento ");
+  });
+
+
+}
+
+function editar(id, nombre, matricula, grupo, telefono) {
+
+
+  document.getElementById("telefono").value = telefono;
+  document.getElementById("nombre").value = nombre;
+  document.getElementById("matricula").value = matricula;
+  document.getElementById("grupo").value = grupo;
+  document.getElementById("fecha").value = fecha;
+
+  const tel = document.getElementById("telefono").value;
+  const nom = document.getElementById("nombre").value;
+  const matri = document.getElementById("matricula").value;
+  const grup = document.getElementById("grupo").value;
+  const fech = document.getElementById("fecha").value;
+
+  boton.innerHTML = 'Editar'; 
+
+  boton.onclick = function () {
+
+    var alumno = db.collection("Alumnos").doc(id);
+
+    return alumno.update({
+      TELEFONO: tel,
+      NOMBRE: nom,
+      MATRICULA: matri,
+      GRUPO: grup,
+      FECHA: fech
+    })
+      .then(() => {
+        console.log("Document successfully updated!");
+      })
+      .catch((error) => {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+      });
   }
 
-  /** Map que contiene el texto de escape de los caracteres especiales de
-   * HTML.
-   * @type {Readonly<Map<string, string>> } */
-  const codMap = Object.freeze(new Map([['&', '&amp;'], ['<', '&lt;'],
-  ['>', '&gt;'], ['"', '&quot;'], ["'", '&#039;']]));
 
-  /** Codifica un texto para que escape los caracteres especiales y no se
-   * pueda interpretar como HTML. Esta técnica evita la inyección de código.
-   * @param {string} texto
-   * @returns {string} un texto que no puede interpretarse como HTML. */
-  function cod(texto) {
-    return (texto || "").replace(/[&<>"']/g, letra => codMap.get(letra));
-  }
-
-
-  function eliminar(id){
-    db.collection("Alumnos").doc(id).delete().then(function(){
-      console.log("Se borro el documento");
-    }).catch(function(error){
-      console.log("Error al borrar el documento ");
-    });
-
-
-  }
+}
