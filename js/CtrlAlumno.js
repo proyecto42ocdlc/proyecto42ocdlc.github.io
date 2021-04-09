131b55afb370a6206094cc2050c4db22fbc09629
Osvaldo Cruz <bobsaurio@gmail.com>
2021-04-08 20:09:35 -0500

    Muestra los datos al editar

--- js/CtrlAlumno.js
+++ js/CtrlAlumno.js
@@ -78,13 +78,27 @@ async function guarda(evt) {
         formData, "matricula").trim();  
     const nombre = getString(
       formData, "nombre").trim();
+
+      
+    const telefono = getString(
+      formData, 'telefono').trim();
+
+    const grupo = getString(
+      formData, 'grupo').trim();
+
+    const fecha = getString(
+      formData, 'fecha').trim();
+
     /**
      * @type {
         import("./tipos.js").
                 Alumno} */
     const modelo = {
-      matricula, 
-      nombre
+      matricula,
+      nombre,
+      telefono,
+      grupo,
+      fecha
     };
     await daoAlumno.
       doc(id).
